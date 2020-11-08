fc10d189c2363c3445fefeb7f971c2410d146907
hannah9lee9 <63530185+hannah9lee9@users.noreply.github.com>
2020-11-08 14:59:16 -0800

    Add files via upload

---
+++ grc339/portfolio/mobile.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mobile_drop_menu").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mobile_drop_menu").style.width = "0";
+}
